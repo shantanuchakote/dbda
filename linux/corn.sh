db3e64bca0c98e6941edf33a6a12e7a3fac5606b
shantanuchakote <99878259+shantanuchakote@users.noreply.github.com>
2023-04-01 16:27:56 +0530

    Add files via upload

---
+++ linux/corn.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#cd
+#crontab -e
+#30 11 * * * /home/dbda/project/cron.sh
+cd project
+mkdir source
+mkdir dest
+cd source
+touch {1..10}.txt
+cp /home/dbda/project/source/*.txt /home/dbda/project/dest
+
